9ec31d8b277a7b0a54704969cca00b04b1ff0268
Saarthi Basant Gupta <saarthibasantgupta31@gmail.com>
2025-09-11 11:44:49 +0530

    Delete json.py (name shadower)

--- json.py
+++
@@ -1,6 +0,0 @@
-import json, requests
-
-url = "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=12f1e1829c6ef03b9fa9c2c43aa212b8&units=metric"
-res = requests.get(url)
-js = res.json()
-print(json.dumps(js, indent=4))
